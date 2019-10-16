8ebdabb07ef7722fc8773fe231edbd7203cc4880
Anton S <av.solodovnyk@gmail.com>
2019-10-16 23:56:33 +0300

    added color gen

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -2,12 +2,24 @@ import React from 'react';
 import T from 'prop-types';
 import styles from './Stats.module.css';
 
+const genColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 const Stats = ({ title, stats }) => (
   <section className={styles.statsSection}>
     <h2 className={styles.title}>{title}</h2>
     <ul className={styles.statList}>
       {stats.map(item => (
-        <li key={item.id} className={styles.item}>
+        <li
+          key={item.id}
+          className={styles.item}
+          style={{ backgroundColor: genColor() }}
+        >
           <span className={styles.label}>{item.label}</span>
           <span className={styles.percentage}>{item.percentage}%</span>
         </li>
