df5da016b71619dd7e0475579009f8ca8a89422d
Anton S <av.solodovnyk@gmail.com>
2019-10-16 22:07:59 +0300

    Updates

---
+++ precache-manifest.9e343515e6b999d519046b5c33d09223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a343512ab671f63eb0e0aefd85d9c2fe",
+    "url": "/github.io/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a97ac7dfb85ffa65439f",
+    "url": "/github.io/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9e4cece8189061a64c0e",
+    "url": "/github.io/goit-react-hw-01-components/static/css/main.7e6cd1a0.chunk.css"
+  },
+  {
+    "revision": "a97ac7dfb85ffa65439f",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.9bce511b.chunk.js"
+  },
+  {
+    "revision": "9e4cece8189061a64c0e",
+    "url": "/github.io/goit-react-hw-01-components/static/js/main.0d6ff517.chunk.js"
+  },
+  {
+    "revision": "23657c1784e399de4b89",
+    "url": "/github.io/goit-react-hw-01-components/static/js/runtime-main.9c4f768d.js"
+  }
+]);
