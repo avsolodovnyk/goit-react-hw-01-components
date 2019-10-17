197097513b9b85814ce62e70c64354d4660088af
Anton S <av.solodovnyk@gmail.com>
2019-10-17 23:42:10 +0300

    Updates

---
+++ precache-manifest.0e74e2228e157314e8ceedfc99deef03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c014a48305a14f3e601a48044c3ec70c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a611055175f7e7d38c14",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "abe1b2108b7e7cd48e82",
+    "url": "/goit-react-hw-01-components/static/css/main.07466cbd.chunk.css"
+  },
+  {
+    "revision": "a611055175f7e7d38c14",
+    "url": "/goit-react-hw-01-components/static/js/2.9eeb2579.chunk.js"
+  },
+  {
+    "revision": "abe1b2108b7e7cd48e82",
+    "url": "/goit-react-hw-01-components/static/js/main.4bea2408.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.1008faa5d0f9976b6477e6c12bbf03cf.js"
+  "/goit-react-hw-01-components/precache-manifest.0e74e2228e157314e8ceedfc99deef03.js"
 );
 
 self.addEventListener('message', (event) => {
